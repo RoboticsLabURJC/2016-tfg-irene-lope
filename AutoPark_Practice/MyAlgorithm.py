d6855706f405956eb1f7024691db0570d00c7ead
ilope236 <irenelope236@hotmail.com>
2016-12-15 11:05:05 +0100

    Actualización

---
+++ AutoPark_Practice/MyAlgorithm.py
@@ -0,0 +1,127 @@
+import numpy as np
+import threading
+import time
+from datetime import datetime
+import jderobot
+import math
+from Target import Target
+from Parser import Parser
+
+time_cycle = 80
+
+class MyAlgorithm(threading.Thread):
+
+    def __init__(self, cameraL, cameraR, pose3d, laser, motors):
+        self.cameraL = cameraL
+        self.cameraR = cameraR
+        self.pose3d = pose3d
+        self.laser = laser
+        self.motors = motors
+
+        self.imageRight=None
+        self.imageLeft=None
+
+        # Car direction
+        self.carx = 0.0
+        self.cary = 0.0
+
+        # Obstacles direction
+        self.obsx = 0.0
+        self.obsy = 0.0
+
+        # Average direction
+        self.avgx = 0.0
+        self.avgy = 0.0
+
+        # Current target
+        self.targetx = 0.0
+        self.targety = 0.0
+
+        self.stop_event = threading.Event()
+        self.kill_event = threading.Event()
+        self.lock = threading.Lock()
+        threading.Thread.__init__(self, args=self.stop_event)
+
+        # Init targets
+        parser = Parser('targets.json')
+        self.targets = parser.getTargets()
+
+    def getNextTarget(self):
+        for target in self.targets:
+            if target.isReached() == False:
+                return target
+
+        return None
+
+    def setRightImageFiltered(self, image):
+        self.lock.acquire()
+        self.imageRight=image
+        self.lock.release()
+
+
+    def setLeftImageFiltered(self, image):
+        self.lock.acquire()
+        self.imageLeft=image
+        self.lock.release()
+
+    def getRightImageFiltered(self):
+        self.lock.acquire()
+        tempImage=self.imageRight
+        self.lock.release()
+        return tempImage
+
+    def getLeftImageFiltered(self):
+        self.lock.acquire()
+        tempImage=self.imageLeft
+        self.lock.release()
+        return tempImage
+
+    def getCarDirection(self):
+        return (self.carx, self.cary)
+
+    def getObstaclesDirection(self):
+        return (self.obsx, self.obsy)
+
+    def getAverageDirection(self):
+        return (self.avgx, self.avgy)
+
+    def getCurrentTarget(self):
+        return (self.targetx, self.targety)
+
+    def run (self):
+
+        while (not self.kill_event.is_set()):
+           
+            start_time = datetime.now()
+
+            if not self.stop_event.is_set():
+                self.execute()
+
+            finish_Time = datetime.now()
+
+            dt = finish_Time - start_time
+            ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+            #print (ms)
+            if (ms < time_cycle):
+                time.sleep((time_cycle - ms) / 1000.0)
+
+    def stop (self):
+        self.stop_event.set()
+
+    def play (self):
+        if self.is_alive():
+            self.stop_event.clear()
+        else:
+            self.start()
+
+    def kill (self):
+        self.kill_event.set()
+
+
+    def execute(self):
+        self.currentTarget=self.getNextTarget()
+        self.targetx = self.currentTarget.getPose().x
+        self.targety = self.currentTarget.getPose().y
+
+        # TODO
+
