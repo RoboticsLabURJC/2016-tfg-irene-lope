74a27c13d68328c757e751aa1a026799f0f68dee
ilope236 <irenelope236@hotmail.com>
2017-01-17 14:03:58 +0100

    modificada funcion drawlaser

--- AutoPark_Practice/gui/widgets/mapWidget.py
+++ AutoPark_Practice/gui/widgets/mapWidget.py
@@ -21,6 +21,7 @@
 from PyQt5.QtWidgets import QWidget, QGridLayout
 from PyQt5.QtGui import QPen, QPainter
 from PyQt5.QtCore import QPoint, QPointF, pyqtSignal, Qt
+import numpy as np
 import math
 
 class MapWidget(QWidget):
@@ -52,8 +53,8 @@ class MapWidget(QWidget):
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.white)
         self.setPalette(p)
-        self.resize(500,500)
-        self.setMinimumSize(500,500)
+        self.resize(300,300)
+        self.setMinimumSize(700,500)
         
     def paintEvent(self, e):
         _width = self.width()
@@ -67,9 +68,12 @@ class MapWidget(QWidget):
         painter.translate(QPoint(_width/2, _height/2))
 
         # Draw laser
-        self.drawLaser1(painter)
-        self.drawLaser2(painter)
-        self.drawLaser3(painter)
+        colorLaser1 = Qt.blue
+        colorLaser2 = Qt.green
+        colorLaser3 = Qt.red
+        self.drawLaser1(painter, colorLaser1,-50)
+        self.drawLaser2(painter, colorLaser2)
+        self.drawLaser3(painter, colorLaser3)
 
 
         # Draw car
@@ -82,40 +86,52 @@ class MapWidget(QWidget):
         pen = QPen(Qt.black, 1)
         painter.setPen(pen)
 
+        # Connectors
+        #painter.drawLine(QPointF(-carsize/5,carsize/5),QPointF(carsize/5, carsize/5))
+
         # Chassis
-        painter.fillRect(-carsize/2, -carsize, carsize, 2*carsize, Qt.yellow)
+        painter.fillRect(-carsize/2, -carsize,carsize,2*carsize,Qt.yellow)
 
         # Tires
-        painter.fillRect(-carsize/2, -carsize, carsize/5, 2*carsize/5, Qt.black)
-        painter.fillRect(carsize/2, -carsize, -carsize/5, 2*carsize/5, Qt.black)
-        painter.fillRect(-carsize/2, carsize - 2*carsize/5, carsize/5, 2*carsize/5, Qt.black)
-        painter.fillRect(carsize/2, carsize - 2*carsize/5, -carsize/5, 2*carsize/5, Qt.black)
+        painter.fillRect(-carsize/2,-carsize,carsize/5,2*carsize/5,Qt.black)
+        painter.fillRect(carsize/2,-carsize,-carsize/5,2*carsize/5,Qt.black)
+        painter.fillRect(-carsize/2,carsize-2*carsize/5,carsize/5,2*carsize/5,Qt.black)
+        painter.fillRect(carsize/2,carsize-2*carsize/5,-carsize/5,2*carsize/5,Qt.black)
 
-    def drawLaser1(self, painter):
-        pen = QPen(Qt.blue, 2)
+
+    def RT1(self, d0, d1):
+        RT = np.array([[math.cos(d1), 0, 0], [math.sin(d1), 0, 0], [0, 0, 1]])
+        return RT
+
+    def drawLaser1(self, painter, color, traslate):
+        pen = QPen(color, 2)
         painter.setPen(pen)
         for d in self.laser1:
-            px = -d[0]*math.sin(d[1])*self.scale
-            py = d[0]*math.cos(d[1])*self.scale
-            painter.drawLine(QPointF(0,-50),QPointF(py, px-50))
+            #px = d[0]*math.cos(d[1])*self.scale
+            #py = -d[0]*math.sin(d[1])*self.scale
+            #poses = np.array([[math.cos(d[1]), 0, 0], [math.sin(d[1]), 0, 0], [0, 0, 1]])*np.array([[d[0]], [-d[0]], [1]]) *self.scale
+            RT = self.RT1(d[0], d[1])
+            poses = RT*np.array([[d[0]], [-d[0]], [1]]) *self.scale
+            painter.drawLine(QPointF(0,traslate),QPointF(poses[0][0], poses[1][0]+traslate))
 
 
-    def drawLaser2(self, painter):
-        pen = QPen(Qt.green, 2)
+    def drawLaser2(self, painter, color):
+        pen = QPen(color, 2)
         painter.setPen(pen)
         for d in self.laser2:
-            px = d[0]*math.sin(d[1])*self.scale
-            py = -d[0]*math.cos(d[1])*self.scale
-            painter.drawLine(QPointF(0,50),QPointF(py, px+50))
+            px = -d[0]*math.cos(d[1])*self.scale
+            py = d[0]*math.sin(d[1])*self.scale
+            painter.drawLine(QPointF(0,50),QPointF(px, py+50))
             
 
-    def drawLaser3(self, painter):
-        pen = QPen(Qt.red, 2)
+    def drawLaser3(self, painter, color):
+        pen = QPen(color, 2)
         painter.setPen(pen)
         for d in self.laser3:
-            px = -d[0]*math.cos(d[1])*self.scale
-            py = -d[0]*math.sin(d[1])*self.scale
-            painter.drawLine(QPointF(-25,0),QPointF(py-25, px))
+            px = -d[0]*math.sin(d[1])*self.scale
+            py = -d[0]*math.cos(d[1])*self.scale
+            painter.drawLine(QPointF(-25,0),QPointF(px-25, py))
+            
 
     def setCarArrow(self, x, y):
         self.carx = x
