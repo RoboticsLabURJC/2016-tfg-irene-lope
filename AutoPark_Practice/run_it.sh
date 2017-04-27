b327fde89d3302aa010c53eee0f184c60cb0d46f
ilope236 <irenelope236@hotmail.com>
2017-04-27 19:49:37 +0200

    python 2

--- AutoPark_Practice/run_it.sh
+++ AutoPark_Practice/run_it.sh
@@ -6,17 +6,16 @@
 # License: GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
 
 world=autopark.world
-#world=circuit.world
 
 gzserver --verbose --minimal_comms $world &
 sleep 5 # up to 20 for circuit.world
 
 [ "$1" = "GUI" ] && gzclient &
 
-python3 autopark.py --Ice.Config=autopark.cfg &
-
-python3 referee.py --Ice.Config=autopark.cfg
+python2 autopark.py --Ice.Config=autopark.cfg &
+#python3 autopark.py --Ice.Config=autopark.cfg
+python2 referee.py --Ice.Config=autopark.cfg
 
 killall gzserver
-killall python3
+killall python2
 [ "$1" = "GUI" ] && killall gzclient
