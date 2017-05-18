0e384771fb2103f6c09c6c4eb5e8971a837ec9ee
ilope236 <irenelope236@hotmail.com>
2017-05-18 20:48:59 +0200

    Solo una camara

--- Stop_Practice/gui/widgets/cameraWidget.py
+++ Stop_Practice/gui/widgets/cameraWidget.py
@@ -2,21 +2,54 @@
 from PyQt5 import QtGui,QtCore
 import cv2
 
-
 class CameraWidget:
     IMG_WIDTH=320
     IMG_HEIGHT=240
 
     def __init__(self,winParent):
         self.winParent=winParent
-        self.labelImageLeft=winParent.imageLeft
-        self.labelImageRight=winParent.imageRight
-        self.labelImageRightFiltered = winParent.imageRightFiltered
-        self.labelImageLeftFiltered = winParent.imageLeftFiltered
+        self.labelImage=winParent.image
+        self.labelImageFiltered = winParent.imageFiltered
 
 
     def updateImage(self):
 
+        
+        img = self.winParent.getCamera().getImage()
+        if img is not  None:
+            resized = cv2.resize(img,(self.IMG_WIDTH,self.IMG_HEIGHT))
+            image = QtGui.QImage(resized.data, resized.shape[1], resized.shape[0], resized.shape[1]*resized.shape[2], QtGui.QImage.Format_RGB888);
+            size=QtCore.QSize(img.shape[1],img.shape[0])
+            self.labelImage.setPixmap(QtGui.QPixmap.fromImage(image))
+
+        #print the filtered images
+        
+        
+        imgFiltered = self.winParent.getAlgorithm().getImageFiltered()
+        if imgFiltered is not None:
+            resized = cv2.resize(imgFiltered,(self.IMG_WIDTH,self.IMG_HEIGHT))
+            image = QtGui.QImage(resized.data, resized.shape[1], resized.shape[0], resized.shape[1]*resized.shape[2], QtGui.QImage.Format_RGB888);
+            size=QtCore.QSize(imgFiltered.shape[1],imgFiltered.shape[0])
+            self.labelImageFiltered.setPixmap(QtGui.QPixmap.fromImage(image))
+        
+
+            
+'''
+class CameraWidget:
+    IMG_WIDTH=320
+    IMG_HEIGHT=240
+
+    def __init__(self,winParent):
+        self.winParent=winParent
+        #self.labelImageLeft=winParent.imageLeft
+        self.labelImageRight=winParent.imageRight
+        self.labelImageRightFiltered = winParent.imageRightFiltered
+        #self.labelImageLeftFiltered = winParent.imageLeftFiltered
+
+
+    def updateImage(self):
+
+        
         imgLeft = self.winParent.getCameraL().getImage()
         if imgLeft is not  None:
             resized = cv2.resize(imgLeft,(self.IMG_WIDTH,self.IMG_HEIGHT))
@@ -24,6 +57,7 @@ class CameraWidget:
             size=QtCore.QSize(imgLeft.shape[1],imgLeft.shape[0])
             #self.label.resize(size)
             self.labelImageLeft.setPixmap(QtGui.QPixmap.fromImage(image))
+        
 
         imgRight = self.winParent.getCameraR().getImage()
         if imgRight is not None:
@@ -35,7 +69,8 @@ class CameraWidget:
 
 
         #print the filtered images
-
+        
+        
         imgLeftFiltered = self.winParent.getAlgorithm().getLeftImageFiltered()
         if imgLeftFiltered is not None:
             resized = cv2.resize(imgLeftFiltered,(self.IMG_WIDTH,self.IMG_HEIGHT))
@@ -43,6 +78,7 @@ class CameraWidget:
             size=QtCore.QSize(imgLeftFiltered.shape[1],imgLeftFiltered.shape[0])
             #self.label.resize(size)
             self.labelImageLeftFiltered.setPixmap(QtGui.QPixmap.fromImage(image))
+        
 
         imgRightFiltered = self.winParent.getAlgorithm().getRightImageFiltered()
         if imgRightFiltered is not None:
@@ -51,3 +87,4 @@ class CameraWidget:
             size=QtCore.QSize(imgRightFiltered.shape[1],imgRightFiltered.shape[0])
             #self.label.resize(size)
             self.labelImageRightFiltered.setPixmap(QtGui.QPixmap.fromImage(image))
+'''
