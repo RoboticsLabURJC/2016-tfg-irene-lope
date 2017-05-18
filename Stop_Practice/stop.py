3b8a741facac07ab87352a808e76788a215971d3
ilope236 <irenelope236@hotmail.com>
2017-05-18 20:53:07 +0200

    camara

--- Stop_Practice/stop.py
+++ Stop_Practice/stop.py
@@ -25,6 +25,7 @@ from gui.GUI import MainWindow
 from gui.threadGUI import ThreadGUI
 from parallelIce.motors import Motors
 from parallelIce.pose3dClient import Pose3DClient
+from parallelIce.cameraClient import CameraClient
 from parallelIce.laserClient import LaserClient
 import easyiceconfig as EasyIce
 from MyAlgorithm import MyAlgorithm
@@ -35,12 +36,14 @@ if __name__ == "__main__":
     ic = EasyIce.initialize(sys.argv)
     motors = Motors (ic, "Stop.Motors")
     pose3d = Pose3DClient(ic, "Stop.Pose3D", True)
+    camera = CameraClient(ic, "Stop.Camera", True)
     
-    algorithm=MyAlgorithm(pose3d, camera, kinect, motors)
+    algorithm=MyAlgorithm(pose3d, camera, motors)
 
     app = QApplication(sys.argv)
     myGUI = MainWindow()
     myGUI.setMotors(motors)
+    myGUI.setCamera(camera)
     myGUI.setPose3D(pose3d)
     myGUI.setAlgorithm(algorithm)
     myGUI.show()
