08ef23d51a950f55d670b697e4282cb9fd5de674
ilope236 <irenelope236@hotmail.com>
2018-01-31 13:42:53 +0100

    cambio funcion checkReturnPoint

--- Vacuum_Practice/MyAlgorithm4.py
+++ Vacuum_Practice/MyAlgorithm4.py
@@ -406,13 +406,13 @@ class MyAlgorithm4(threading.Thread):
             eCell = cells[1]
             wCell = cells[2]
             sCell = cells[3]
-            if nCell[0] == 0 and nCell[1] == 0:
+            if nCell == 0:
                 cont += 1
-            if eCell[0] == 0 and eCell[1] == 0:
+            if eCell == 0:
                 cont += 1
-            if wCell[0] == 0 and wCell[1] == 0:
+            if wCell == 0:
                 cont += 1
-            if wCell[0] == 0 and wCell[1] == 0:
+            if wCell == 0:
                 cont += 1
             
             if cont == 0:
