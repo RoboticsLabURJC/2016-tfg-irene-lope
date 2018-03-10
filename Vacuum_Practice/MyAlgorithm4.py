a6cdc5d68091fc5245cb1b5c45cf5a575db839a4
ilope236 <irenelope236@hotmail.com>
2018-03-10 20:16:28 +0100

    Aumentada la dilatacion de obstaculos para el retorno

--- Vacuum_Practice/MyAlgorithm4.py
+++ Vacuum_Practice/MyAlgorithm4.py
@@ -31,7 +31,7 @@ class MyAlgorithm4(threading.Thread):
         self.map_orig = cv2.imread("resources/images/mapgrannyannie.png", cv2.IMREAD_GRAYSCALE)
         self.map_orig = cv2.resize(self.map_orig, (500, 500))
         kernel = np.ones((10, 10), np.uint8)
-        kernelV = np.ones((12, 12), np.uint8)
+        kernelV = np.ones((13, 13), np.uint8)
         self.mapE = cv2.erode(self.map_orig, kernel, iterations=1)
         self.mapEVis = cv2.erode(self.map_orig, kernelV, iterations=1)
         self.mapECopy = self.mapE.copy()
